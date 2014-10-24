1a1e827e792c3a7b2f2e0d4164eaf29341e52cbd
mespina <mespina@acid.cl>
2014-10-24 18:14:02 -0300

    refactor de vistas para simplificar

--- app/assets/javascripts/spree/frontend/spree_one_page_checkout.js
+++ app/assets/javascripts/spree/frontend/spree_one_page_checkout.js
@@ -2,13 +2,12 @@ var disable_steps;
 
 disable_steps = function(all) {
   var elements;
-  elements = all != null ? $(".checkout_content") : $(".checkout_content.disabled-step");
-  elements.addClass("disabled-step");
+  elements = $(".checkout_content.disabled-step");
   elements.find("form input").attr("disabled", "disabled");
 };
 
 Spree.ready(function($) {
-  if (($('#checkout')).is('*')) {
+  if ($('#checkout').is('*')) {
     return disable_steps();
   }
 });
--- app/controllers/frontend/checkout_controller_decorator.rb
+++ app/controllers/frontend/checkout_controller_decorator.rb
@@ -4,7 +4,11 @@ module Spree
 
     private
       def check_logged_in_user
+        # Skip the login step if user already logged in
         redirect_to spree.checkout_path if current_spree_user
+
+        # Skip the login step if not enabled
+        redirect_to spree.checkout_path unless Spree::Auth::Config[:registration_step]
       end
   end
 end
