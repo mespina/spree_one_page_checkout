35ac756aa9ec249d2f22765e5636b01b285d9a79
mespina <mespina@acid.cl>
2014-10-24 15:29:13 -0300

    fix formularios de pasos deshabilitados

--- app/assets/javascripts/spree/frontend/spree_one_page_checkout.js
+++ app/assets/javascripts/spree/frontend/spree_one_page_checkout.js
@@ -1,2 +1,15 @@
-// Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+var disable_steps;
+
+disable_steps = function(all) {
+  var elements;
+  elements = all != null ? $(".checkout_content") : $(".checkout_content.disabled-step");
+  elements.addClass("disabled-step");
+  elements.find("form input").attr("disabled", "disabled");
+  return elements.find("#checkout-summary, .errorExplanation").hide();
+};
+
+Spree.ready(function($) {
+  if (($('#checkout')).is('*')) {
+    return disable_steps();
+  }
+});
