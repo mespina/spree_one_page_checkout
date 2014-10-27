b377d9790379ced05064d228a04a9f6b77582598
mespina <mespina@acid.cl>
2014-10-27 14:16:47 -0300

    fix en registro dentro del checkout cuando se seteo el email en la orden

--- app/controllers/frontend/checkout_controller_decorator.rb
+++ app/controllers/frontend/checkout_controller_decorator.rb
@@ -5,7 +5,7 @@ module Spree
     private
       def check_logged_in_user
         # Skip the login step if user already logged in
-        redirect_to spree.checkout_path if current_spree_user
+        redirect_to spree.checkout_path if current_spree_user or current_order.email.present?
 
         # Skip the login step if not enabled
         redirect_to spree.checkout_path unless Spree::Auth::Config[:registration_step]
